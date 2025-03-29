886677d96f14f5c8a7d0afa214cadf28051b7d37
kentjohnbranzuela <158124803+kentjohnbranzuela@users.noreply.github.com>
2025-03-29 23:14:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for reaching out! I will get back to you soon.");
-});
 document.addEventListener("DOMContentLoaded", function () {
     const elements = document.querySelectorAll(".animated");
     elements.forEach((el, index) => {
@@ -11,4 +7,3 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 200);
     });
 });
-
